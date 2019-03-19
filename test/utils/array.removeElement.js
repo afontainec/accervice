b1f47c384948bfea14d60b4637535add90ebfb09
Antonio Fontaine <afontaine@uc.cl>
2019-03-19 18:29:22 -0300

    remove element from array

--- test/utils/array.removeElement.js
+++ test/utils/array.removeElement.js
@@ -26,18 +26,47 @@ describe('Array: remove element', () => { // eslint-disable-line
   });
 
   it('remove duplicated element',  (done) => { // eslint-disable-line
-    done('NOT IMPLEMENTED');
+    const array = ['a', 'b', 'c', 'b'];
+    const element = 'b';
+    const input = Utils.cloneJSON(array);
+    const result = Utils.Array.removeElement(input, element);
+    assert.isArray(result);
+    assert.equal(result.length, array.length - 1);
+    assert.equal(array[0], result[0]);
+    assert.equal(array[2], result[1]);
+    assert.equal(array[3], result[2]);
+    done();
   });
 
   it('remove unexisiting element',  (done) => { // eslint-disable-line
-    done('NOT IMPLEMENTED');
+    const array = ['a', 'b', 'c'];
+    const element = 'd';
+    const input = Utils.cloneJSON(array);
+    const result = Utils.Array.removeElement(input, element);
+    assert.deepEqual(array, result);
+    done();
   });
 
   it('array is undefined',  (done) => { // eslint-disable-line
-    done('NOT IMPLEMENTED');
+    try {
+      const element = 'd';
+      Utils.Array.removeElement(null, element);
+      done('SHOULD NOT GET HERE');
+    } catch (err) {
+      done();
+    }
   });
 
-  it('element is undefined',  (done) => { // eslint-disable-line
-    done('NOT IMPLEMENTED');
+  it('element is null',  (done) => { // eslint-disable-line
+    const array = ['a', null, 'c'];
+    const element = null;
+    const input = Utils.cloneJSON(array);
+    const result = Utils.Array.removeElement(input, element);
+    assert.isArray(result);
+    assert.equal(result.length, array.length - 1);
+    assert.equal(result.indexOf(element), -1);
+    assert.equal(array[0], result[0]);
+    assert.equal(array[2], result[1]);
+    done();
   });
 });
