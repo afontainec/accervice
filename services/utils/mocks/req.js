7ee8dca6c0ee2f5b42738219b8d0a85c5ff41b79
antonio <afontaine@uc.cl>
2019-12-18 18:57:37 -0300

    mock req as class

--- services/utils/mocks/req.js
+++ services/utils/mocks/req.js
@@ -1,57 +1,51 @@
 
 
-let authenticated = false;
-const headers = {
-  'user-agent': 'UA',
-  'x-forwarded-for': '10.5.50.1',
-};
-const secure = false;
+class Req {
 
-function isAuthenticated() {
-  return authenticated;
+  constructor() {
+    this.headers = {
+      'user-agent': 'UA',
+      'x-forwarded-for': '10.5.50.1',
+    };
+    this.secure = false;
+    this.authenticated = false;
+  }
+
+  isAuthenticated() {
+    return this.authenticated;
+  }
+
+
+  login(user) {
+    if (!user) return;
+    this.authenticated = true;
+    this.user = user;
+  }
+
+  logout() {
+    this.authenticated = false;
+    this.user = undefined;
+  }
+
+  setParam(key, value) {
+    this.params = this.params || {};
+    this.params[key] = value;
+  }
+
+  setBody(key, value) {
+    this.body = this.body || {};
+    this.body[key] = value;
+  }
+
+  setQuery(key, value) {
+    this.query = this.query || {};
+    this.query[key] = value;
+  }
+
+  static generate() {
+    return new Req();
+  }
 }
 
 
-function login(user) {
-  if (!user) return;
-  authenticated = true;
-  this.user = user;
-}
-
-function logout() {
-  authenticated = false;
-  this.user = undefined;
-}
-
-function setParam(key, value) {
-  this.params = this.params || {};
-  this.params[key] = value;
-}
-
-function setBody(key, value) {
-  this.body = this.body || {};
-  this.body[key] = value;
-}
-
-function setQuery(key, value) {
-  this.query = this.query || {};
-  this.query[key] = value;
-}
-
-const generate = () => {
-  return {
-    isAuthenticated,
-    login,
-    logout,
-    setBody,
-    setParam,
-    setQuery,
-    headers,
-    secure,
-  };
-};
-
-
-module.exports = {
-  generate,
-};
+module.exports = Req;
