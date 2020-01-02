d17318078e0e72c04f6fff8a3432e1afa3b8acaf
antonio <afontaine@uc.cl>
2020-01-02 13:36:04 -0300

    http mock test

---
+++ services/utils/mocks/angularjs/http.js
@@ -0,0 +1,37 @@
+const requestify = require('requestify');
+
+let host = '';
+
+const setHost = (input) => {
+  host = input;
+};
+
+const parseUrl = (url) => {
+  return url && url.startsWith('/') ? host + url : url;
+};
+
+const get = async (url, config) => {
+  url = parseUrl(url);
+  const data = await requestify.get(url, config);
+  return { data };
+};
+
+const post = async (url, body) => {
+  url = parseUrl(url);
+  const data = await requestify.post(url, body);
+  return { data };
+};
+
+const put = async (url, body) => {
+  url = parseUrl(url);
+  const data = await requestify.put(url, body);
+  return { data };
+};
+
+
+module.exports = {
+  setHost,
+  get,
+  post,
+  put,
+};
