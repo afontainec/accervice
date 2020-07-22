c12b56ed3e8a54e7ba91907492552d36f01a2c69
Diego Bordeu <dbordeu@uc.cl>
2020-07-22 15:47:33 -0400

    linter(auto fix)

--- test/accionet/timezone/resolveMeAlways.js
+++ test/accionet/timezone/resolveMeAlways.js
@@ -1,31 +1,31 @@
 /* global describe, it */
 // During the test the env variable is set to test
 process.env.NODE_ENV = 'test';
-const Accionet = require('../../../services/accionet')
+const Accionet = require('../../../services/accionet');
 
 // Our parent block
 describe('Accionet.resolveMeAlways', () => { // eslint-disable-line max-lines-per-function
-    it('happy path: when promise is resolved', (done) => {
-      Accionet.resolveMeAlways(resolvePromise()).then(() => {
-        done();
-      }).catch(() => {
-        done('should not be here');
-      })
-    })
-    it('happy path: when promise is rejected', (done) => {
-      Accionet.resolveMeAlways(rejectPromise()).then(() => {
-        done()
-      }).catch(() => {
-        done('should not be here');
-      })
-    })
-    it('happy path: when promise is rejected and alertMail = true', (done) => {
-      Accionet.resolveMeAlways(rejectPromise(), true).then(() => {
-        done()
-      }).catch(() => {
-        done('should not be here');
-      })
-    })
+  it('happy path: when promise is resolved', (done) => {
+    Accionet.resolveMeAlways(resolvePromise()).then(() => {
+      done();
+    }).catch(() => {
+      done('should not be here');
+    });
+  });
+  it('happy path: when promise is rejected', (done) => {
+    Accionet.resolveMeAlways(rejectPromise()).then(() => {
+      done();
+    }).catch(() => {
+      done('should not be here');
+    });
+  });
+  it('happy path: when promise is rejected and alertMail = true', (done) => {
+    Accionet.resolveMeAlways(rejectPromise(), true).then(() => {
+      done();
+    }).catch(() => {
+      done('should not be here');
+    });
+  });
 });
 
 
@@ -33,14 +33,14 @@ const resolvePromise = () => {
   return new Promise((resolve) => {
     setTimeout(() => {
       return resolve();
-    }, 100)
-  })
-}
+    }, 100);
+  });
+};
 
 const rejectPromise = () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       return reject();
-    }, 100)
-  })
-}
+    }, 100);
+  });
+};
